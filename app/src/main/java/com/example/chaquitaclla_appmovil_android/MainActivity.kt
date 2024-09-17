b1f4ee708c20d3182748b5a01e185a33c091d51a
fdaniel13s <u20221c628@upc.edu.pe>
2024-09-17 18:29:47 -0500

    chore: added button to access at the statistics layout.

--- app/src/main/java/com/example/chaquitaclla_appmovil_android/MainActivity.kt
+++ app/src/main/java/com/example/chaquitaclla_appmovil_android/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.chaquitaclla_appmovil_android
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Set up the button to navigate to com.example.chaquitaclla_appmovil_android.com.example.chaquitaclla_appmovil_android.StaticsActivity
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            startActivity(newIntent(this))
+        }
     }
+
+
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, StatisticsActivity::class.java)
+        }
+    }
+
 }
