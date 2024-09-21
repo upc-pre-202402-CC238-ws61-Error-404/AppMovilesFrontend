8e63a94e667da3fb82d35aba3cd038f97d234a86
fdaniel13s <u20221c628@upc.edu.pe>
2024-09-20 22:02:24 -0500

    feat: added resources xml to icons of bottom-navigation-bar

---
+++ app/src/main/java/com/example/chaquitaclla_appmovil_android/BaseActivity.kt
@@ -0,0 +1,39 @@
+package com.example.chaquitaclla_appmovil_android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+open class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.navigation_forum -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                R.id.navigation_profile -> {
+                    // Respond to navigation item 3 click
+                    true
+                }
+                R.id.navigation_history -> {
+                    // Respond to navigation item 4 click
+                    true
+                }
+                R.id.navigation_statistics -> {
+                    // Respond to navigation item 5 click
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
