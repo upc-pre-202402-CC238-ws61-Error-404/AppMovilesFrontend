06cd12107bdff95b66ac2004ba3d4d7f5e7b2bb4
AMNER LEVI LLAMO SANCHEZ <u20221c376@upc.edu.pe>
2024-09-21 00:25:14 -0500

    feat(forum): added answers service.

--- app/src/main/java/com/example/chaquitaclla_appmovil_android/forum/interfaces/ForumApi.kt
+++ app/src/main/java/com/example/chaquitaclla_appmovil_android/forum/interfaces/ForumApi.kt
@@ -3,11 +3,12 @@ package com.example.chaquitaclla_appmovil_android.forum.interfaces
 import com.example.chaquitaclla_appmovil_android.forum.beans.Answer
 import com.example.chaquitaclla_appmovil_android.forum.beans.Question
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ForumApi {
     @GET("forum/questions")
     suspend fun getQuestions(): List<Question>
 
-    @GET("forum/questions/{questionId}/answers")
-    suspend fun getAnswersByQuestionId(questionId: Int): List<Answer>
+    @GET("forum/question/{questionId}/answers")
+    suspend fun getAnswersByQuestionId(@Path("questionId") questionId: Int): List<Answer>
 }
