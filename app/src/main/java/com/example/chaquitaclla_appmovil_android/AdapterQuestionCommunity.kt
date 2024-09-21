09a62b982db9e1b7582341761518f9ef873f49a1
AMNER LEVI LLAMO SANCHEZ <u20221c376@upc.edu.pe>
2024-09-21 00:26:37 -0500

    feat(forum): added question adapter.

--- app/src/main/java/com/example/chaquitaclla_appmovil_android/AdapterQuestionCommunity.kt
+++ app/src/main/java/com/example/chaquitaclla_appmovil_android/AdapterQuestionCommunity.kt
@@ -1,5 +1,6 @@
 package com.example.chaquitaclla_appmovil_android
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -17,6 +18,13 @@ class AdapterQuestionCommunity(val questionsList: List<Question>):RecyclerView.A
     override fun onBindViewHolder(holder: QuestionCommunityViewHolder, position: Int) {
         val item = questionsList[position]
         holder.render(item)
+
+        holder.itemView.setOnClickListener {
+            val context = holder.itemView.context
+            val intent = Intent(context,AnswersActivity::class.java)
+            intent.putExtra("question", item)
+            context.startActivity(intent)
+        }
     }
 
 }
