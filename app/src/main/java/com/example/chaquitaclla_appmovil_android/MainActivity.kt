63d6699eab92e1149f4863b0da603694dd2eeb75
AMNER LEVI LLAMO SANCHEZ <u20221c376@upc.edu.pe>
2024-09-26 12:31:28 -0500

    feat(iam-forum): docs.

--- app/src/main/java/com/example/chaquitaclla_appmovil_android/MainActivity.kt
+++ app/src/main/java/com/example/chaquitaclla_appmovil_android/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.chaquitaclla_appmovil_android
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.chaquitaclla_appmovil_android.forum.activitys.ForumManagementActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val buttonForum: Button = findViewById(R.id.btnForum)
+
+        buttonForum.setOnClickListener {
+            startActivity(newIntentForum(this))
+        }
+    }
+
+    companion object {
+        fun newIntentForum(context: Context): Intent{
+            return Intent(context, ForumManagementActivity::class.java)
         }
     }
 }
